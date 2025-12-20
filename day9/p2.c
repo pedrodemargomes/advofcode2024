d74eabca36223fd9882b9ea06dbd19f63562be08
Pedro Demarchi Gomes <pedrodemargomes@gmail.com>
2025-12-20 12:25:51 -0300

    day9

--- day9/p2.c
+++ day9/p2.c
@@ -5,6 +5,7 @@ struct space {
 	int id;
 	int size;
 	int isFile;
+	int visited;
 };
 
 struct array {
@@ -16,8 +17,28 @@ void insert(struct array *a, struct space n) {
 	a->a[a->size++] = n;
 }
 
+void insertAt(struct array *a, struct space n, int index) {
+	a->size++;
+	for(int i = a->size-1; i > index; i--) {
+		a->a[i] = a->a[i-1];
+	}
+	a->a[index] = n;
+}
+
 void printSpace(struct space s) {
 	printf("id: %d size: %d isFile: %d\n", s.id, s.size, s.isFile);
+}
+
+void printDisk(struct array *d) {
+	for (int i = 0; i < d->size; i++) {
+		for (int j = 0; j < d->a[i].size; j++) {
+			if (!d->a[i].isFile)
+				printf(".");
+			else
+				printf("%d", d->a[i].id);
+		}
+	}
+	printf("\n");
 }
 
 struct array disk;
@@ -38,7 +59,7 @@ int main(int argv, char *argc[]) {
 		};
 		insert(&disk, s);
 
-		printSpace(s);
+		//printSpace(s);
 	}
 
 	int end = disk.size-1;
@@ -46,44 +67,48 @@ int main(int argv, char *argc[]) {
 		end--;	
 	printf("end: %d\n", end);
 	
-	int pos = 0;
-	unsigned long checksum = 0;
 
-	int f = 0;
-	while (f <= end) {
-		if (f % 2 == 0) {
-			for(int k = 0; k < disk.a[f].size; k++) {
-				printf("%d", disk.a[f].id);
-				checksum += disk.a[f].id * pos++;						
-			}
-			f++;
-		} else {
-			if (disk.a[f].size == disk.a[end].size) {
-				for(int k = 0; k < disk.a[end].size; k++) {
-					printf("%d", disk.a[end].id);
-					checksum += disk.a[end].id * pos++;
+	while (end > 0) {
+		if (!disk.a[end].isFile && !disk.a[end].visited)
+			goto cont;
+		disk.a[end].visited = 1;
+		for (int i = 0; i < end;i++) {
+			if (!disk.a[i].isFile && disk.a[i].size >= disk.a[end].size) {
+				disk.a[i].isFile = 1;
+				disk.a[i].id = disk.a[end].id;
+				int szEnd = disk.a[end].size;
+				disk.a[end].isFile = 0;
+				if (disk.a[i].size > szEnd) {
+					struct space rest = {
+						.id = -1,
+						.isFile = 0,
+						.size = disk.a[i].size - szEnd
+					};
+					
+					insertAt(&disk, rest, i+1);
 				}
-
-				f++;
-				end -= 2;
-			} else if (disk.a[f].size < disk.a[end].size) {
-				for(int k = 0; k < disk.a[f].size; k++) {
-					printf("%d", disk.a[end].id);
-					checksum += disk.a[end].id * pos++;
-				}
-				disk.a[end].size -= disk.a[f].size;
-				f++;
-			} else {
-				for(int k = 0; k < disk.a[end].size; k++) {
-					printf("%d", disk.a[end].id);
-					checksum += disk.a[end].id * pos++;
-				}
-				disk.a[f].size -= disk.a[end].size;
-				end -= 2;
-
+				disk.a[i].size = szEnd;
+				
+				break;
 			}
 		}
+
+	cont:
+		end--;
 	}
+
+	//printDisk(&disk);
+
+	int pos = 0;
+	unsigned long checksum = 0;
+	for (int i = 0; i < disk.size; i++) {
+		for (int j = 0; j < disk.a[i].size; j++) {
+			if (disk.a[i].isFile)
+				checksum += pos*disk.a[i].id;
+			pos++;
+		}
+	}
+
 	printf("\nchecksum: %lu\n", checksum);
 
 	return 0;
